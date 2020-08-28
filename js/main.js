55dc50640ed397c863aa6199d79e6f247e1e114a
pranaykumargoud <pranaygoud36@gmail.com>
2020-08-28 16:34:25 +0530

    Added a slide page

--- js/main.js
+++ js/main.js
@@ -308,6 +308,6 @@
 
 }());
 
-const text=document.querySelector(".typing-text"),words=["a problem solver.","a developer.","an avid learner.", "a sketch artist."];
+const text=document.querySelector(".typing-text"),words=["Hi, I am Pranay kumar.", "I am a problem solver.","I am a developer.","I am an avid learner.", "I am a sketch artist."];
 
 function setTyper(e,t){const n=75,r=2e3,o=0,s=1;var c,l=o,a=0,i=0;function u(){c=setInterval(v,n)}function v(){const n=t[a];l==o?++i==n.length&&(l=s,clearInterval(c),setTimeout(u,r)):l==s&&0==--i&&(i=0,l=o,++a==t.length&&(a=0));const v=n.substring(0,i);e.textContent=v}u()}setTyper(text,words);
