6055bc49a52aded12f9350a77573a550ddbf4bf6
yadavakrishna <pranayyadav950@gmail.com>
2025-03-15 23:01:08 +0530

    new changes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // Enables static export
   distDir: "out", // Output folder
-  basePath: "", // No subpath since this is a user GitHub page
+  basePath: "/pranaybathini.github.io", // Adjust this to match your repo name
+  assetPrefix: "/pranaybathini.github.io/",
   images: {
     unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
     remotePatterns: [
